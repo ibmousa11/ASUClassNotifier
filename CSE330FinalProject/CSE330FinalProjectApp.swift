72b7a23458d6f6332edf5d07f9201e9de8c5765a
ibmousa11 <ibmousa11@gmail.com>
2022-11-10 13:46:46 -0800

    Initial Commit

---
+++ CSE330FinalProject/CSE330FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSE330FinalProjectApp.swift
+//  CSE330FinalProject
+//
+//  Created by Ibrahim Mousa on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CSE330FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSE330FinalProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CSE330FinalProject
+//
+//  Created by Ibrahim Mousa on 11/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
